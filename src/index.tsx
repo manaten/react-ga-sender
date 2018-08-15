9804ddaabb3e87eacefb7a50ffdcc112335ddd57
manaten <manaten@manaten.net>
2018-08-16 02:50:15 +0900

    feat(GASender): multiple tracking id

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { Component } from 'react';
 
-const GA_ID = '';
-
 if (typeof window === 'object') {
   // https://developers.google.com/analytics/devguides/collection/analyticsjs/
   /* eslint-disable */
@@ -16,12 +14,23 @@ if (typeof window === 'object') {
     m.parentNode.insertBefore(a, m);
   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
   /* eslint-enable */
-  (window as any).ga('create', GA_ID, 'auto');
 }
 
-function sendToGA(page: string): void {
-  (window as any).ga('set', 'page', page);
-  (window as any).ga('send', 'pageview', page);
+
+const trackers: {[trackingId: string]: string} = {};
+function getGATrackerName(trackingId: string): string {
+  if (trackers[trackingId]) {
+    return trackers[trackingId];
+  }
+  const trackerName = `tracker_${Object.keys(trackers).length + 1}`;
+  (window as any).ga('create', trackingId, 'auto', trackerName);
+  trackers[trackingId] = trackerName;
+  return trackerName;
+}
+
+function sendToGA(page: string, trackerName: string): void {
+  (window as any).ga('set', 'page', page, trackerName);
+  (window as any).ga('send', 'pageview', page, trackerName);
 }
 
 type Location = {
@@ -32,18 +41,19 @@ type Location = {
 const getPageFromLocation = ({ pathname, search }: Location): string => pathname + search;
 
 type GASenderProps = {
+  trackingId: string,
   location: Location
 };
 
 export class GASender extends Component<GASenderProps> {
   componentDidMount() {
-    sendToGA(getPageFromLocation(this.props.location));
+    sendToGA(getPageFromLocation(this.props.location), getGATrackerName(this.props.trackingId));
   }
 
   componentDidUpdate(prevProps: GASenderProps) {
     const newPage = getPageFromLocation(this.props.location);
     if (getPageFromLocation(prevProps.location) !== newPage) {
-      sendToGA(newPage);
+      sendToGA(newPage, getGATrackerName(this.props.trackingId));
     }
   }
 
